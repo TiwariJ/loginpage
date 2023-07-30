d51ad07bdf34f8f3d7ced614796055caa5eee438
Priya Tiwari <87890054+TiwariJ@users.noreply.github.com>
2023-07-30 21:21:46 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>SignUp</title>
+  </head>
+  <body>
+    <?php require'partial/_nav.php'?>
+    <div class="container">
+        <h1 class="text-center">Signup to our website</h1>
+        <form action="/cwh/signup.php"method="post">
+  <div class="form-group col-md-6">
+    <label for="username">Username</label>
+    <input type="email" class="form-control" id="username" name= "username"aria-describedby="emailHelp" >
+  </div>
+  <div class="form-group col-md-6">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="cpassword1"> Confirm Password</label>
+    <input type="password" class="form-control" id="cpassword" name="cpassword">
+    <small id="email.help" class="form-text text-muted">We will never share your email with anyone else.</small>
+  </div>
+  
+  <button type="submit" class="btn btn-primary">SignUp</button>
+</form>
+
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
